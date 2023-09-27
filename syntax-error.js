bc6ea8849aa1ff13e222851997337699261415d4
unknown <gausulislam5@gmail.com>
2023-09-27 23:01:16 +0600

    update eeor

---
+++ syntax-error.js
@@ -0,0 +1,16 @@
+function isBigger(num){
+    if(num>5){
+        return true;
+    }
+    else{
+        return false
+    }
+}
+const result=isBigger(11);
+console.log(result);
+
+
+const sum= (a,b) =>{
+    const res=sum(3,5)
+
+}
