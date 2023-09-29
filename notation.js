5df12fcb34f0d8f3f5135e5609772165dcaa475d
unknown <gausulislam5@gmail.com>
2023-09-29 21:03:45 +0600

    update notation

---
+++ notation.js
@@ -0,0 +1,18 @@
+const person={
+    name:"lal nil holud mia",
+    profession:"Traffic surgeon",
+    age:39,
+    25:'summer',
+    address:'shaka'
+}
+// const profileI=person.profession;
+const prof2=person['profession'];
+console.log(prof2);
+// console.log(profileI);
+
+const pName='profession';
+const profile2=person[pName];
+console.log(profile2);
+
+const season =person[25];
+console.log(season);
