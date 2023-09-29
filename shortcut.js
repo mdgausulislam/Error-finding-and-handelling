894b839bc61c5392cb97735b12401e45b506e29a
unknown <gausulislam5@gmail.com>
2023-09-29 19:31:59 +0600

    update ternary operator

--- shortcut.js
+++ shortcut.js
@@ -1,3 +1,11 @@
 const inputStr='560';
 const input=+inputStr;
 console.log(input);
+
+const isActive=true;
+const showUser=()=>console.log("display user");
+const hideUser=()=>console.log("hide user");
+
+// isActive?showUser(): hideUser();
+isActive && showUser();
+isActive || showUser();
