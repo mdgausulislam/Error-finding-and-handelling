8c16deeea04d5ac23ac986c54dfbc9b0a5c52cf6
unknown <gausulislam5@gmail.com>
2023-09-29 20:49:15 +0600

    update local storage

--- storage.js
+++ storage.js
@@ -1,1 +1,14 @@
 localStorage.setItem('userId',8795214)
+
+const addToLocalStorage=()=>{
+    const idInput=document.getElementById('storage-Id');
+    const id=idInput.value;
+    const valueInput=document.getElementById('storage-value');
+    const value=valueInput.value;
+
+  if(id && value ){
+    localStorage.setItem(id,value);
+  }
+    idInput.value=' ';
+    valueInput.value='';
+}
